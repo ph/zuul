8b79af9fcb87ce2379dad158dfae1bfc215f516c
Pier-Hugues Pellerin <phpellerin@gmail.com>
2025-04-02 09:36:00 -0400

    feat: make the window float in front of other and adjust size

--- src/main.rs
+++ src/main.rs
@@ -302,15 +302,20 @@ async fn perform_response(response: Response) -> Result<(), ZuulErr> {
 fn main() -> iced::Result {
     iced::application(Application::title, Application::update, Application::view)
         .window(iced::window::Settings {
-            position: Position::Centered,
-            platform_specific: PlatformSpecific {
-                application_id: String::from("zuul"),
-                override_redirect: true,
+	    decorations: false,
+	    size: (400.0, 120.0).into(),
+	    min_size: Some((400.0, 115.0).into()),
+	    max_size: Some((400.0, 180.0).into()),
+	    position: Position::Centered,
+	    resizable: false,
+	    level: window::Level::AlwaysOnTop,
+	    platform_specific: PlatformSpecific {
+		application_id: String::from("zuul"),
+		override_redirect: true,
             },
             ..Default::default()
         })
         .theme(Application::theme)
-        .window_size((400.0, 400.0))
         .subscription(Application::subscription)
         .run_with(Application::new)
 }
