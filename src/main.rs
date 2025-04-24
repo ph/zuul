eb0bd28a476cb1149f1b617a7ff436020457201f
ph <phpellerin@gmail.com>
2025-04-24 09:28:57 -0400

    cleanup from the generated template

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: {{ license }}
+use tracing::info;
 
 mod app;
 mod config;
 mod i18n;
 
 fn main() -> cosmic::iced::Result {
+    init_logging();
+
+    info!("zuul ({})", <app::Zuul as cosmic::Application>::APP_ID);
+
     // Get the system's preferred languages.
     let requested_languages = i18n_embed::DesktopLanguageRequester::requested_languages();
 
@@ -19,5 +24,35 @@ fn main() -> cosmic::iced::Result {
     );
 
     // Starts the application's event loop with `()` as the application's flags.
-    cosmic::app::run::<app::AppModel>(settings, ())
+    cosmic::app::run::<app::Zuul>(settings, ())
+}
+fn init_logging() {
+    use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+
+    // Initialize logger
+    #[cfg(feature = "console")]
+    if std::env::var("TOKIO_CONSOLE").as_deref() == Ok("1") {
+        std::env::set_var("RUST_LOG", "trace");
+        console_subscriber::init();
+    }
+
+    let filter_layer = EnvFilter::try_from_default_env().unwrap_or(if cfg!(debug_assertions) {
+        EnvFilter::new(format!("warn,{}=debug", env!("CARGO_CRATE_NAME")))
+    } else {
+        EnvFilter::new("warn")
+    });
+
+    let fmt_layer = fmt::layer().with_target(false);
+
+    if let Ok(journal_layer) = tracing_journald::layer() {
+        tracing_subscriber::registry()
+            .with(journal_layer)
+            .with(filter_layer)
+            .init();
+    } else {
+        tracing_subscriber::registry()
+            .with(fmt_layer)
+            .with(filter_layer)
+            .init();
+    }
 }
